fa93b35b48b8cf7faa3c64b2e5dc4f7299b2bc7c
Josh Ballanco <jballanc@gmail.com>
2014-04-08 22:49:31 +0300

    Add periodic data generators and some tests
    This adds DataGen classes to generate Sine wave and Square wave
    distributions.

---
+++ lib/datagen/square.rb
@@ -0,0 +1,15 @@
+require_relative 'periodic'
+
+module DataGen
+  # Generates a basic square-wave data set
+  class Square < Periodic
+    def raw_value_at_time(t)
+      i = (t % @period) / @period
+      if i < 0.5
+        0.5 * @amplitude
+      else
+        -0.5 * @amplitude
+      end
+    end
+  end
+end
---
+++ test/lib/datagen/sin_test.rb
@@ -0,0 +1,27 @@
+require_relative '../../test_helper'
+
+require 'datagen/sin'
+
+describe DataGen::Sin do
+  it 'has a default period of 1 (1 Hz)' do
+    curve = DataGen::Sin.new
+    3.times do
+      t = rand
+      curve.value_at(t).must_be_close_to curve.value_at(t + 1)
+    end
+  end
+
+  it 'can be created with a different period' do
+    curve = DataGen::Sin.new(period: 3)
+    3.times do
+      t = rand
+      curve.value_at(t).must_be_close_to curve.value_at(t + 3)
+    end
+  end
+
+  it 'has "amplitude" peak-to-trough height' do
+    curve = DataGen::Sin.new(amplitude: 4, period: 2, delay: -0.5)
+    height = curve.value_at(0) - curve.value_at(1)
+    height.must_be_close_to 4
+  end
+end
