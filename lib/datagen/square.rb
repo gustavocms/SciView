181a270c176909db8c1b64002b22dae11524bf12
Josh Ballanco <jballanc@gmail.com>
2014-04-09 22:21:35 +0300

    Fix method name

--- lib/datagen/square.rb
+++ lib/datagen/square.rb
@@ -3,7 +3,7 @@ require_relative 'periodic'
 module DataGen
   # Generates a basic square-wave data set
   class Square < Periodic
-    def raw_value_at_time(t)
+    def raw_value_at(t)
       i = (t % @period) / @period
       if i < 0.5
         0.5 * @amplitude
