d99ea415a08cec37485893e3cd945fdeb1f5c9e8
Josh Ballanco <jballanc@gmail.com>
2014-04-09 23:04:03 +0300

    Add percent error and refactor value adjustment

--- lib/datagen/base.rb
+++ lib/datagen/base.rb
@@ -2,18 +2,25 @@ module DataGen
   # Base class for data generators. Contains attributes and helper functions that are used
   # by all generators.
   class Base
-    attr_accessor :offset,   # Constant factor added to each data point (y-value)
-                  :delay,    # Time delay factor added to each data point (x-value)
-                  :tolerance # Random factor added or subtracted from each data point
+    attr_accessor :offset,    # Constant factor added to each data point (y-value)
+                  :delay,     # Time delay factor added to each data point (x-value)
+                  :tolerance, # Random factor added or subtracted from each data point
+                  :pct_error  # Random error as a percent of the raw value
 
-    def initialize(offset: 0, delay: 0, tolerance: 0)
+    def initialize(offset: 0, delay: 0, tolerance: 0, pct_error: 0)
       @offset    = offset
       @delay     = delay
       @tolerance = tolerance
+      @pct_error = pct_error
     end
 
     def adjust_value(val)
-      val + @offset + (@tolerance * (rand - 0.5))
+      tolerance_range = -@tolerance..@tolerance
+      percent_error = -(@pct_error * val)..(@pct_error * val)
+      val +
+        @offset +
+        Random.rand(tolerance_range) +
+        Random.rand(percent_error)
     end
 
     def value_at(t)
