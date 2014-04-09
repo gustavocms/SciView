98eb39c2f65f28dba55c8abaee80adee0d54dc3e
Josh Ballanco <jballanc@gmail.com>
2014-04-09 22:21:47 +0300

    Generators for power law and normal distributions

---
+++ lib/datagen/normal.rb
@@ -0,0 +1,22 @@
+require_relative 'base'
+
+module DataGen
+  # Returns values from a standard normal distribution curve
+  class Normal < Base
+    attr_accessor :value, # Value of the data at the mean (not accounting for any offset)
+                  :stddev # Standard deviation of the normal distribution
+
+    def initialize(value: 1, stddev: 1)
+      @vaule  = value
+      @stddev = stddev
+    end
+
+    def raw_value_at(t)
+      # We'll use the `delay` attribute as the normal mean
+      exponent = - (t - @delay)**2 / (2 * @stddev**2)
+      factor = @value / (@stddev * Math.sqrt(2 * Math::PI))
+
+      factor * Math.exp(exponent)
+    end
+  end
+end
