f0207f9d65d1b981ac09b74ddc84e6ef631307ac
Josh Ballanco <jballanc@gmail.com>
2014-04-23 22:35:28 +0300

    A few small style tweaks for tests and one fix
    The fix is to add the Devise helpers to ActionController tests so that
    they don't error out when Devise view helpers are called

--- test/lib/datagen/line_test.rb
+++ test/lib/datagen/line_test.rb
@@ -1,4 +1,4 @@
-require_relative '../../test_helper'
+require 'test_helper'
 require 'datagen/line'
 
 describe DataGen::Line do
--- test/lib/datagen/sum_test.rb
+++ test/lib/datagen/sum_test.rb
@@ -1,4 +1,4 @@
-require_relative '../../test_helper'
+require 'test_helper'
 require 'datagen/sum'
 require 'datagen/line'
 
