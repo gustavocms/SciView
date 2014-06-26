d9eaf8628940b90cce231046486d597e35006038
Gustavo Souza <gustavo.souza@toptal.com>
2014-06-26 18:27:45 -0300

    Replace respond_with for render json:

--- ruby_app/app/controllers/attributes_controller.rb
+++ ruby_app/app/controllers/attributes_controller.rb
@@ -2,10 +2,10 @@ class AttributesController < ApplicationController
   respond_to :json
 
   def create
-    respond_with Dataset.update_attribute(params[:dataset_id], params[:attribute], params[:value]), :location => root_url
+    render json: Dataset.update_attribute(params[:dataset_id], params[:attribute], params[:value])
   end
 
   def destroy
-    respond_with Dataset.remove_attribute(params[:dataset_id], params[:id])
+    render json: Dataset.remove_attribute(params[:dataset_id], params[:id])
   end
 end
