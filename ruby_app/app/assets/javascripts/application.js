ae0ea1f67bfa6ed431f749149d19f2878baba7fa
Gustavo Souza <gustavo.souza@toptal.com>
2014-07-01 21:11:17 -0300

    Configuring Bootstrap

--- ruby_app/app/assets/javascripts/application.js
+++ ruby_app/app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require angular
 //= require angular-route
 //= require angular-resource
+//= require bootstrap-sprockets
 //= require sciview
 //= require stream_layers
 //= require charts/multiple
