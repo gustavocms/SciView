10a6b788f8f1a1e13e8a907363a8080891a2b281
zvkemp <zvkemp@gmail.com>
2014-07-09 16:05:59 -0700

    csv upload worker

--- ruby_app/app/workers/csv_upload_worker.rb
+++ ruby_app/app/workers/csv_upload_worker.rb
@@ -1,7 +1,10 @@
 class CsvUploadWorker
   include Sidekiq::Worker
 
-  def perform(series_name, tempfile)
-
+  def perform(series_name, tempfile_path)
+    CsvToTempoDb.new(tempfile_path).tap do |csv|
+      csv.series_name = series_name
+      csv.save!
+    end
   end
 end
