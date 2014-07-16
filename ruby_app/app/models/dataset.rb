4cf98554962172bdb16249c529923ef481869c11
zvkemp <zvkemp@gmail.com>
2014-07-16 10:54:52 -0700

    refactor dataset - make instances retrieve multiple series

--- ruby_app/app/models/dataset.rb
+++ ruby_app/app/models/dataset.rb
@@ -81,35 +81,80 @@ class Dataset
   end
 
   attr_accessor :start, :stop, :count
-  attr_reader :client, :key
+  attr_reader :client, :keys
 
-  def initialize(series_name)
-    @client = tempodb_client
-    @key = URI.decode(series_name)
+  # def initialize(series_names)
+  #   @client = tempodb_client
+  #   @keys   = Array(series_names).map {|sn| URI.decode(sn) }
 
-    # Find start and stop times based on first and last single values
-    past = Time.utc(1999, 1, 1)
-    future = Time.utc(2020, 1, 1)
-    @start = @client.single_value(@key, ts: past, direction: 'after').data.ts
-    @stop = @client.single_value(@key, ts: future, direction: 'before').data.ts
+  #   # Find start and stop times based on first and last single values
+  #   past = Time.utc(1999, 1, 1)
+  #   future = Time.utc(2020, 1, 1)
+  #   @start = @client.single_value(@key, ts: past, direction: 'after').data.ts
+  #   @stop = @client.single_value(@key, ts: future, direction: 'before').data.ts
+  # end
+
+  # def as_json(opts = {})
+  #   if count
+  #     opts[:rollup_period] = "PT#{"%.2f" % ((@stop - @start) / count)}S"
+  #     opts[:rollup_function] = 'mean'
+  #   end
+
+  #   [{ 
+  #      key: @key, 
+  #      values: Sampling::RandomSample.sample(tempodb_client.read_data(@key, @start, @stop, opts).to_a, 500)
+  #   }]
+  # end
+  
+  # series: a hash of the form { series_1: 'sample_abcdef123' }
+  # options: start
+  #          stop
+  #          count
+  attr_reader :series_names, :client, :options
+  def initialize(series, opts = {})
+
+    @series_names = series.values
+    @start        = opts.fetch(:start){ Time.utc(1999,1,1) }
+    @stop         = opts.fetch(:stop){ Time.utc(2020,1,1) }
+    @options      = { keys: series_names, count: opts[:count] }.select {|_,v| v }
   end
 
   def summary
     @summary ||= DatasetSummary.new(client.get_summary(key, start, stop))
   end
-
-
-  def as_json(opts = {})
-    if count
-      opts[:rollup_period] = "PT#{"%.2f" % ((@stop - @start) / count)}S"
-      opts[:rollup_function] = 'mean'
-    end
-
-    [{ 
-       key: @key, 
-       values: Sampling::RandomSample(tempodb_client.read_data(@key, @start, @stop, opts), 500)
-    }]
+  
+  def to_hash
+    return_hash
   end
+
+  private
+
+  def series(sn)
+    { key: "#{sn['key']}", values: [], tags: sn['tags'], attributes: sn['attributes'] }
+  end
+
+  def cursor
+    @cursor ||= tempodb_client.read_multi(start, stop, options)
+  end
+
+  def return_hash
+    @return_hash ||= {}.tap do |hash|
+      cursor['series'].each { |sn| hash["#{sn['key']}"] = series(sn) }
+      cursor.each do |datapoint|
+        datapoint.value.each do |key, value|
+          hash[key][:values] << { value: value, ts: datapoint.ts }
+        end
+      end
+
+      hash.each do |_, series|
+        t = Time.now
+        puts "SAMPLING STARTED..."
+        series[:values] = Sampling::RandomSample.sample(series[:values], 2000)
+        puts "SAMPLING ENDED (#{Time.now - t} seconds)"
+      end
+    end
+  end
+
 end
 
 require 'forwardable'
