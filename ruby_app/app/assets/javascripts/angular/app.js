e4d7bd889b42c74f3dd812c7664e7e0c1f33a4e7
Gustavo Souza <gustavo.souza@toptal.com>
2014-07-17 17:56:29 -0300

    Change angular router for UI-Router for multiple and nested views

--- ruby_app/app/assets/javascripts/angular/app.js
+++ ruby_app/app/assets/javascripts/angular/app.js
@@ -1,10 +1,52 @@
 (function() {
-    angular.module('sciViewApp', [
+    var app = angular.module('sciViewApp', [
+        'ui.router',
         'ui.bootstrap',
         'ui.utils',
-        'metadataRoutes',
         'metadataDirectives',
         'metadataServices',
         'metadataControllers'
     ]);
+
+    app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+        function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+            $stateProvider
+                .state('multiChart', {
+                    url: "/charts/multiple",
+                    views: {
+                        "saveChart": {
+                            templateUrl: "/assets/save_chart.html"
+                        },
+                        "metadata": {
+                            templateUrl: "/assets/metadata.html",
+                            controller: 'MetadataController',
+                            resolve: {
+                                // looking for the parameters on the hidden field to inject on the controller
+                                seriesParams: function () {
+                                    var seriesParams = JSON.parse($('#chartSeries').text());
+                                    return seriesParams;
+                                }
+                            }
+                        }
+                    }
+
+                })
+                .state('singleChart', {
+                    url: "/charts/:chartId",
+                    templateUrl: "/assets/metadata.html",
+                    controller: 'MetadataController',
+                    resolve: {
+                        // looking for the parameters on the hidden field to inject on the controller
+                        seriesParams: function () {
+                            var seriesParams = JSON.parse($('#chartSeries').text());
+                            return seriesParams;
+                        }
+                    }
+                });
+
+            // configure html5 to get links working on jsfiddle
+            $locationProvider.html5Mode(true);
+        }]);
+
 })();
