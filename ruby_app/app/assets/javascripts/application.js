e322a898d5a5e12da275c4fecf22073aef8794cb
Gustavo Souza <gustavo.souza@toptal.com>
2014-08-04 18:40:11 -0300

    Adding angular-sanitize dependency to allow html injection from controllers

--- ruby_app/app/assets/javascripts/application.js
+++ ruby_app/app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require angular/angular
 //= require angular-route/angular-route
 //= require angular-resource/angular-resource
+//= require angular-sanitize/angular-sanitize
 //= require angular-ui-bootstrap-bower/ui-bootstrap-tpls
 //= require angular-ui-utils/ui-utils
 //= require angular-ui-router
