62d148b1bbc3d9c9eaa66b26c5f75eb1dd589230
zvkemp <zvkemp@gmail.com>
2014-08-04 14:27:35 -0700

    make data structure legible

--- ruby_app/app/assets/javascripts/sciview/services/DataSets.js
+++ ruby_app/app/assets/javascripts/sciview/services/DataSets.js
@@ -7,33 +7,15 @@ app.service('DataSets', function($http, $cookieStore, $state) {
                 id: '0',
                 title: 'Data Set 1',
                 batch: [
+                  { title: 'Pressure', chart: 'assets/graph_1.svg', channel: [
                     {
-                        title: 'Pressure',
-                        chart: 'assets/graph_1.svg',
-                        channel: [
-                            {
-                                title: 'Pressure Sensors',
-                                group: [
-                                    {
-                                        title: 'fuel_pressure-d',
-                                        category: 'pressure',
-                                        key: {
-                                            color: '#FF00D8',
-                                            style: "dashed"
-                                        }
-                                    },                                    {
-                                        title: 'oil_pressure-3a',
-                                        category: 'pressure',
-                                        key: {
-                                            color: '#FFF81D',
-                                            style: "solid"
-                                        }
-                                    }
-
-                                ]
-                            }
-                        ]
-                    },                    
+                      title: 'Pressure Sensors', group: [
+                        { title: 'fuel_pressure-d', category: 'pressure', key: { color: '#FF00D8', style: "dashed" } },
+                        { title: 'oil_pressure-3a' category: 'pressure', key: { color: '#FFF81D', style: "solid" } }
+                      ]
+                    }
+                      ]
+                  },                    
                     {
                         title: 'Speed',
                         chart: 'assets/graph_2.svg',
