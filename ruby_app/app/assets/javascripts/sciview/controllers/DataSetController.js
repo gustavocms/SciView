2ac54b36b1899a83cdfa1e70030aa0054f3c9dbf
zvkemp <zvkemp@gmail.com>
2014-08-05 10:21:02 -0700

    mock dataset request

--- ruby_app/app/assets/javascripts/sciview/controllers/DataSetController.js
+++ ruby_app/app/assets/javascripts/sciview/controllers/DataSetController.js
@@ -5,12 +5,19 @@ app.controller('DataSetController', [
     '$stateParams',
     '$state',
     'DataSets',
-    function($scope, $rootScope, $location, $stateParams, $state, DataSets) {
+    '$http',
+    function($scope, $rootScope, $location, $stateParams, $state, DataSets, $http) {
 
         // Get all Data Sets        
         $scope.data_sets = DataSets.getDataSets();
+
+
         // Set current Data Set
-        $scope.current_data_set = $scope.data_sets[$stateParams.dataSetId];
+        setCurrentDataSet = function() {
+          $scope.current_data_set = $scope.data_sets[$stateParams.dataSetId];
+        };
+
+        setCurrentDataSet();
         
         // Make $state available in $scope
         $scope.$state = $state;        
@@ -27,6 +34,24 @@ app.controller('DataSetController', [
             else 
                 obj.state = "retracted";
         };
+
+
+        // TEMPORARY
+        $http.get('datasets/multiple.json?series_1=sample_0a3803_1405960534&series_2=sample_640636_1404681975')
+          .success(function(data) { 
+            console.log(data.data);
+            $scope.data_sets.push(
+              {
+                id: '3',
+                title: 'TEMP TITLE',
+                batch: [
+                  { title: 'Pressure', chart: 'assets/graph_1.svg', channel: [
+                    { title: 'Pressure Sensors', group: [{ title: 'fuel_pressure-d', category: 'pressure', key: { color: '#FF00D8', style: "dashed" } }]}
+                  ]}
+                ]
+              }          
+            );
+          });
     }
 
 ]);
--- ruby_app/app/assets/javascripts/sciview/services/DataSets.js
+++ ruby_app/app/assets/javascripts/sciview/services/DataSets.js
@@ -4,55 +4,24 @@ app.service('DataSets', function($http, $cookieStore, $state) {
     DataSets.getDataSets = function() {
         return [
             {
-                id: '0',
-                title: 'Data Set 1',
-                batch: [
-                  { title: 'Pressure', chart: 'assets/graph_1.svg', channel: [
-                    {
-                      title: 'Pressure Sensors', group: [
-                        { title: 'fuel_pressure-d', category: 'pressure', key: { color: '#FF00D8', style: "dashed" } },
-                        { title: 'oil_pressure-3a' category: 'pressure', key: { color: '#FFF81D', style: "solid" } }
-                      ]
-                    }
-                      ]
-                  },                    
-                    {
-                        title: 'Speed',
-                        chart: 'assets/graph_2.svg',
-                        channel: [
-                            {
-                                title: 'Pressure Sensors',
-                                group: [
-                                    {
-                                        title: 'fuel_pressure-d',
-                                        category: 'pressure',
-                                        key: {
-                                            color: '#AC00FF',
-                                            style: "solid"
-                                        }
-                                    },                                    {
-                                        title: 'oil_pressure-3a',
-                                        category: 'pressure',
-                                        key: {
-                                            color: '#FF00D8',
-                                            style: "solid"
-                                        }
-                                    }
-
-                                ]
-                            },
-                            {
-                                title: 'random_sensor',
-                                category: 'pressure',
-                                key: {
-                                    color: '#00E7FF',
-                                    style: "solid"
-                                }
-                            }
-                        ]
-                    }
-                ]
-                // Dummy chart for now
+              id: '0',
+              title: 'Data Set 1',
+              batch: [
+                { title: 'Pressure', chart: 'assets/graph_1.svg', channel: [
+                  { title: 'Pressure Sensors', group: [
+                    { title: 'fuel_pressure-d', category: 'pressure', key: { color: '#FF00D8', style: "dashed" } },
+                    { title: 'oil_pressure-3a', category: 'pressure', key: { color: '#FFF81D', style: "solid" } }
+                  ]}
+                ]},                    
+                { title: 'Speed', chart: 'assets/graph_2.svg', channel: [
+                  { title: 'Pressure Sensors', group: [
+                    { title: 'fuel_pressure-d', category: 'pressure', key: { color: '#AC00FF', style: "solid" } },
+                    { title: 'oil_pressure-3a', category: 'pressure', key: { color: '#FF00D8', style: "solid" } }
+                  ]},
+                  { title: 'random_sensor', category: 'pressure', key: { color: '#00E7FF', style: "solid" } }
+                ]}
+              ]
+              // Dummy chart for now
             },            
             {
                 id: '1',
