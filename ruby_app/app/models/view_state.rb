5619d3a68699dcb29c443758012b390eaf395eb3
zvkemp <zvkemp@gmail.com>
2014-08-07 09:42:18 -0700

    document view state

--- ruby_app/app/models/view_state.rb
+++ ruby_app/app/models/view_state.rb
@@ -1,2 +1,32 @@
+# This model is designed to be served as a JSON structure through the API.
+# columns:
+# id (Int)
+# user_id (Int)
+# charts (Postgres JSON)
+#
+# The structure follows the UIDataset model in the angular app:
+#
+# { 
+#   id: 0
+#   user_id: 1
+#   charts: [
+#     { 
+#       title: 'chart title'
+#       channels: [
+#         {
+#           state: 'expanded'
+#           title: 'channel title'
+#           series: [
+#             { title: "sample_012345", key: { color: 'red', style: 'solid' }}
+#           ]
+#         }
+#       ]
+#     }
+#   ]
+# }
+#  
+# This is fetched by angular's ViewState resource and built into the appropriate
+# objects by SciView.Models.UIDataset.deserialize.
+#
 class ViewState < ActiveRecord::Base
 end
