954bcef38def9b62291ff351b5d3aac63dc83123
zvkemp <zvkemp@gmail.com>
2014-08-12 11:00:23 -0700

    functioning "update" action for ViewState

--- ruby_app/app/controllers/api/v1/view_states_controller.rb
+++ ruby_app/app/controllers/api/v1/view_states_controller.rb
@@ -14,12 +14,27 @@ class Api::V1::ViewStatesController < ApplicationController
   end
 
   def update
-    puts params.inspect
+    ViewState.find(params[:id]).tap do |view_state|
+      # NOTE: Postgres' hstore columns behave somewhat unexpectedly with regards to
+      # ActiveRecord's dirty attribute checking. This explicit update is the easiest way to workaround.
+      view_state.charts_will_change!
+      view_state.charts = params[:charts]
+      view_state.title  = params[:title]
+      if view_state.save!
+        respond_with "true"
+      else
+        respon_with view_state.errors.as_json
+      end
+    end
   end
 
   private
 
   def view_state
     @view_state ||= ViewState.find(params[:id])
+  end
+
+  def view_state_params
+    params.permit(:title, :charts)
   end
 end
---
+++ ruby_app/test/controllers/api/v1/view_states_controller_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+
+class Api::V1::ViewStatesControllerTest < ActionController::TestCase
+  before { sign_in test_user }
+
+  def view_state
+    @view_state ||= ViewState.create(title: "TestViewState", charts: [])
+  end
+
+  def updated_view_state
+    ViewState.find(view_state)
+  end
+
+  def update_params
+    {
+      "id"=>"#{view_state.id}", 
+      "title"=>"Updated TestViewState", 
+      "charts"=>[
+        {
+          "title"=>"Untitled Chart", 
+          "channels"=>[
+            {
+              "title"=>"default channel", 
+              "state"=>"expanded", 
+              "series"=>[]
+            }
+          ]
+        }
+      ]
+    }
+  end
+
+
+
+  test 'update' do
+    assert_equal view_state.charts, []
+    put :update, update_params.merge({ format: :json })
+
+    updated_view_state.tap do |vs|
+      assert_equal vs.charts.count, 1
+      assert_equal vs.title, "Updated TestViewState"
+    end
+  end
+end
+
