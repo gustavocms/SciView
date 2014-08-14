345700967d4b152de33b05ab0e5d4f75a6e125e9
Gustavo Souza <gustavo.souza@toptal.com>
2014-08-14 17:44:33 -0300

    Adding new metadata working

--- ruby_app/app/assets/javascripts/sciview/controllers/MetadataControllers.js
+++ ruby_app/app/assets/javascripts/sciview/controllers/MetadataControllers.js
@@ -1,117 +1,54 @@
 (function() {
     var module = angular.module('sv.ui.controllers.metadata', []);
 
-    module.controller('MetadataController', ['$scope', '$log', 'MetadataService', 'ModalService', 'SeriesTagsService', 'SeriesAttributesService',
-        function ($scope, $log, MetadataService, ModalService, SeriesTagsService, SeriesAttributesService) {
+    module.controller('MetadataController', ['$scope', '$log', '$q', 'MetadataService', 'SeriesTagsService', 'SeriesAttributesService',
+        function ($scope, $log, $q, MetadataService, SeriesTagsService, SeriesAttributesService) {
 
-            //$scope.channel.title from parent controller
-            $scope.parameters = {series_1: $scope.series.title};
+            $scope.metaState = {
+                //$scope.channel.title from parent controller
+                parameters: {series_1: $scope.series.title},
+                showForm: false
+            };
 
             //wait for the promise to succesfully finish
-            MetadataService.query($scope.parameters, function(seriesList) {
+            MetadataService.query($scope.metaState.parameters, function(seriesList) {
                 $scope.seriesData = seriesList[0];
             });
 
-            $scope.addMetadata = function(series) {
-                var result = ModalService.showModal({
-                    controller: 'NewMetadataController',
-                    templateUrl: '/assets/charts/metadata/add_metadata_form.html',
-                    resolve: {
-                        series: function () {
-                            return series;
-                        }
-                    }
-                }, {});
-
-                result.then(function (newMetadata) {
-                    $log.info(newMetadata);
-                });
-            };
-
-            $scope.removeAttribute = function(series, key) {
-                var result = ModalService.showModal({}, {
-                    actionButtonText: 'Delete',
-                    headerText: 'Remove attribute?',
-                    bodyText: 'Are you sure you want to delete this attribute?<br><br><strong>' + key + ':' + series.attributes[key] + '</strong>'
-                });
-
-                result.then(function () {
-                    //ajax call
-                    SeriesAttributesService.delete({
-                            seriesId: series.key,
-                            attributeId: key
-                        }, {},
-                        //onSuccess promise function
-                        function () {
-                            //update scope removing object
-                            delete series.attributes[key];
-                        });
-                });
-            };
-
-            $scope.removeTag = function(series, tag) {
-                var result = ModalService.showModal({}, {
-                    actionButtonText: 'Delete',
-                    headerText: 'Remove tag?',
-                    bodyText: 'Are you sure you want to delete this tag?<br><br><strong>' + tag + '</strong>'
-                });
-
-                result.then(function () {
-                    //ajax call
-                    SeriesTagsService.delete({
-                            seriesId: series.key,
-                            tagId: tag
-                        }, {},
-                        //onSuccess promise function
-                        function () {
-                            //update scope removing object
-                            series.tags.splice(series.tags.indexOf(tag), 1);
-                        });
-                });
-            };
-        }
-    ]);
-
-    module.controller('NewMetadataController', ['$scope', '$modalInstance', '$log', '$q', 'series', 'SeriesTagsService', 'SeriesAttributesService',
-        function ($scope, $modalInstance, $log, $q, series, SeriesTagsService, SeriesAttributesService) {
+            //form model
             $scope.newMetadata = {
-                series: series,
                 tag: "",
                 attribute: "",
                 value: ""
             };
 
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
+            $scope.saveMetaChannel = function () {
+                var promises = [];
 
-            $scope.ok = function (metadataForm) {
-                if (metadataForm.$valid) {
+                if ($scope.newMetadata.tag.length > 0) {
+                    //asynchronous saving
+                    var promise = saveNewTag();
 
-                    var promises = [];
+                    //add the promise to the array to wait for all to complete
+                    promises.push(promise);
+                };
 
-                    if ($scope.newMetadata.tag.length > 0) {
-                        //asynchronous saving
-                        var promise = saveNewTag();
+                if ($scope.newMetadata.attribute.length > 0) {
+                    //asynchronous saving
+                    var promise = saveNewAttribute();
 
-                        //add the promise to the array to wait for all to complete
-                        promises.push(promise);
-                    };
+                    //add the promise to the array to wait for all to complete
+                    promises.push(promise);
+                };
 
-                    if ($scope.newMetadata.attribute.length > 0) {
-                        //asynchronous saving
-                        var promise = saveNewAttribute();
-
-                        //add the promise to the array to wait for all to complete
-                        promises.push(promise);
-                    };
-
-                    //wait for all saving to be completed and then closes.
-                    //defining finally as a string for IE compatibility (https://docs.angularjs.org/api/ng/service/$q)
-                    $q.all(promises)['finally'](function() {
-                        $modalInstance.close($scope.newMetadata);
-                    });
-                }
+                //wait for all saving to be completed and then closes the form and cleanup the model.
+                //defining finally as a string for IE compatibility (https://docs.angularjs.org/api/ng/service/$q)
+                $q.all(promises)['finally'](function() {
+                    $scope.metaState.showForm = false;
+                    $scope.newMetadata.tag = "";
+                    $scope.newMetadata.attribute = "";
+                    $scope.newMetadata.value = "";
+                });
             };
 
             function saveNewTag() {
@@ -119,11 +56,13 @@
 
                 //ajax call
                 var promise = SeriesTagsService.save(
-                    {seriesId: $scope.newMetadata.series.key}, tagData,
+                    {seriesId: $scope.seriesData.key}, tagData,
                     //onSuccess promise function
                     function () {
-                        //update scope with new object
-                        $scope.newMetadata.series.tags.push(tagData.tag);
+                        if ($scope.seriesData.tags.indexOf(tagData.tag) == -1) {
+                            //update scope with new object
+                            $scope.seriesData.tags.push(tagData.tag);
+                        }
                     }).$promise;
 
                 //return promise of the saving call
@@ -138,17 +77,16 @@
 
                 //ajax call
                 var promise = SeriesAttributesService.save(
-                    {seriesId: $scope.newMetadata.series.key}, attrData,
+                    {seriesId: $scope.seriesData.key}, attrData,
                     //onSuccess promise function
                     function () {
                         //update scope with new object
-                        $scope.newMetadata.series.attributes[attrData.attribute] = attrData.value;
+                        $scope.seriesData.attributes[attrData.attribute] = attrData.value;
                     }).$promise;
 
                 //return promise of the saving call
                 return promise;
             };
-
         }
     ]);
 })();
