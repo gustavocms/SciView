1b2e8fb02728c9e21234dbb1076c645d09459644
Gustavo Souza <gustavo.souza@toptal.com>
2014-08-18 20:59:46 -0300

    Fix some missing references

--- ruby_app/app/assets/javascripts/sciview/services/Observations.js
+++ ruby_app/app/assets/javascripts/sciview/services/Observations.js
@@ -6,7 +6,7 @@ app.service('Observations', function($http, $cookieStore, $state) {
             {
                 data_set: "launch_39.2",
                 data_point: "C",
-                img: "../img/prof-1.png",
+                img: "/assets/prof-1.png",
                 author: "Blake Benthall",
                 created: "1288323623006",
                 observation: "This doesn't look good, it could be X or Y, but not Z."
@@ -14,7 +14,7 @@ app.service('Observations', function($http, $cookieStore, $state) {
             {
                 data_set: "TEST_3A",
                 data_point: "F",
-                img: "../img/prof-2.png",
+                img: "/assets/prof-2.png",
                 author: "Paul Mestemaker",
                 created: "1288323623006",
                 observation: "I think this problem will continue to grow if we don't address immediately. I think we could do Z if we had A and B, but that's up to Stan."
@@ -22,7 +22,7 @@ app.service('Observations', function($http, $cookieStore, $state) {
             {
                 data_set: "TEST_3A",
                 data_point: "F",
-                img: "../img/prof-2.png",
+                img: "/assets/prof-2.png",
                 author: "Paul Mestemaker",
                 created: "1288323623006",
                 observation: "I think this problem will continue to grow if we don't address immediately. I think we could do Z if we had A and B, but that's up to Stan."
@@ -30,7 +30,7 @@ app.service('Observations', function($http, $cookieStore, $state) {
             {
                 data_set: "TEST_3A",
                 data_point: "F",
-                img: "../img/prof-2.png",
+                img: "/assets/prof-2.png",
                 author: "Paul Mestemaker",
                 created: "1288323623006",
                 observation: "I think this problem will continue to grow if we don't address immediately. I think we could do Z if we had A and B, but that's up to Stan."
@@ -38,7 +38,7 @@ app.service('Observations', function($http, $cookieStore, $state) {
             {
                 data_set: "launch_39.2",
                 data_point: "C",
-                img: "../img/prof-1.png",
+                img: "/assets/prof-1.png",
                 author: "Blake Benthall",
                 created: "1288323623006",
                 observation: "This doesn't look good, it could be X or Y, but not Z."
