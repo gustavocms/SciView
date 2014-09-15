fdd875a374b1b739e317868323f62ce3ad8472f2
zvkemp <zvkemp@gmail.com>
2014-09-15 11:52:58 -0700

    cleanup

--- ruby_app/test/lib/tdms_test.rb
+++ ruby_app/test/lib/tdms_test.rb
@@ -11,12 +11,12 @@ describe 'Tdms' do
     Tdms::File.parse(filename).tap do |doc|
       doc.channels.each_with_index do |channel, index|
         YAML.load(File.read("fixtures/tdms_#{Digest::SHA1.hexdigest(channel.path.to_s)}.yml")).tap do |expectation|
-          puts channel.path.to_s
           specify("#{channel.path} path"){ channel.path.to_s.must_equal expectation[:path] }
           specify("#{channel.path} properties"){ channel.properties.must_equal expectation[:properties] }
-          specify { channel.values.to_a.must_equal expectation[:values] }
+          specify("#{channel.path} values"){ channel.values.to_a.must_equal expectation[:values] }
         end
-        ## Generate the files
+
+        ## Generates the files
         #File.open("fixtures/tdms_#{Digest::SHA1.hexdigest(channel.path.to_s)}.yml", 'w') do |file|
           #file.write(YAML.dump(
             #{ path: channel.path.to_s, properties: channel.properties, values: channel.values.to_a }
