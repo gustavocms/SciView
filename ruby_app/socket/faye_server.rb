ea484e10fabbee603ac678e67d5094c345516ce5
zvkemp <zvkemp@gmail.com>
2014-10-13 13:06:24 -0700

    refactor message delegation

--- ruby_app/socket/faye_server.rb
+++ ruby_app/socket/faye_server.rb
@@ -17,37 +17,7 @@ module Sciview
 
     def call(env)
       if Faye::WebSocket.websocket?(env)
-        ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
-        ws.on :open do |event|
-          clients << ws
-          p [:open, clients.length, ws.object_id]
-        end
-
-        ws.on :message do |event|
-          message = JSON.parse(event.data)['message']
-          case message['event']
-          when 'subscribe'
-            room = subscribe(message['resource'],message['id'],ws)
-            p [:message, :subscribe, room.size, message['id'], event.data]
-          when 'update'
-            room = broadcast(message['resource'],message['id'],ws,event.data)
-            p [:message, :update, room.size, message['id'], event.data]
-          else
-            p [:message, message, clients.length, event.data]
-            clients.each {|ws| ws.send(event.data) }
-          end
-        end
-
-        ws.on :close do |event|
-          p [:close, clients.length, ws.object_id, event.code, event.reason]
-          clients.delete(ws)
-          rooms.each {|_, room| room.delete(ws) }
-          ws = nil
-        end
-
-        # Return async Rack response
-        ws.rack_response
-
+        socket_call(env)
       else
         app.call(env)
       end
@@ -55,21 +25,66 @@ module Sciview
 
     private
 
+    # Only allow these event names.
+    # Anything else will be delegated to :_generic_message 
+    SANITIZED_MESSAGE_NAMES = {
+      "subscribe" => :_subscribe,
+      "update"    => :_update
+    }
+
+    def socket_call(env)
+      ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
+      ws.on :open do 
+        clients << ws
+        p [:open, clients.length, ws.object_id]
+      end
+
+      ws.on :message do |event|
+        message = JSON.parse(event.data)['message']
+        sym = SANITIZED_MESSAGE_NAMES.fetch(message['event'], :_generic_message)
+        send(sym, message, ws, event)
+      end
+
+      ws.on :close do |event|
+        p [:close, clients.length, ws.object_id, event.code, event.reason]
+        clients.delete(ws)
+        rooms.each {|_, room| room.delete(ws) }
+      end
+
+      # Return async Rack response
+      ws.rack_response
+    end
+
     def room_for(resource, id)
       rooms[[resource, id]].tap do |room|
         yield room if block_given?
       end
     end
 
-    def subscribe(resource, id, socket)
-      room_for(resource ,id) do |room|
-        room << socket
-      end
+    def _subscribe(message, socket, *)
+      subscribe(message['resource'], message['id'], socket)
     end
 
-    def broadcast(resource, id, originSocket, data)
+    def _update(message, socket, event)
+      broadcast(message['resource'], message['id'], socket, event.data)
+    end
+
+    # NOTE: Is there any reason to do this? This leaves a hole for anyone
+    # to spam all connected clients with whatever data they want.
+    # Logged, but broadcast disabled for now.
+    def _generic_message(message, socket, event)
+      p ["BLOCKED", :message, message, clients.length, event.data]
+      #clients.each {|ws| ws.send(event.data) }
+    end
+
+
+    def subscribe(resource, id, socket)
+      room_for(resource ,id) { |room| room << socket }
+    end
+
+    def broadcast(resource, id, origin_socket, data)
       room_for(resource,id) do |room|
-        room.each {|ws| ws.send(data) if ws != originSocket }
+        room.each {|ws| ws.send(data) unless ws == origin_socket }
       end
     end
   end
