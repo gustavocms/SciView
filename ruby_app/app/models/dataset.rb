a9da4448280394822e3d7231cb6dd21182e30681
zvkemp <zvkemp@gmail.com>
2014-10-17 13:23:12 -0700

    replace method_missing -> def_delegators

--- ruby_app/app/models/dataset.rb
+++ ruby_app/app/models/dataset.rb
@@ -1,5 +1,24 @@
+require 'forwardable'
+
 class Dataset
   class << self
+    extend Forwardable
+
+    # The following should be implemented as class methods on the 
+    # adapter.
+    def_delegators :adapter, 
+      :all,
+      :multiple_series,
+      :update_series,
+      :update_attribute,
+      :remove_attribute,
+      :add_tag,
+      :remove_tag,
+      :for_series,
+      :series_metadata,
+      :multiple_series_metadata
+
+
     def use_adapter(klass)
       @adapter = klass
     end
@@ -12,9 +31,9 @@ class Dataset
 
     DEFAULT_ADAPTER = DatasetAdapters::TempoDBAdapter
 
-    def method_missing(name, *args, &block)
-      adapter.send(name, *args, &block)
-    end
+    #def method_missing(name, *args, &block)
+      #adapter.send(name, *args, &block)
+    #end
 
     def adapter
       @adapter ||= DEFAULT_ADAPTER
