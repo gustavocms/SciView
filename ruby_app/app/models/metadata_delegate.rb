b937b03901cba750d8d6e6e53cfd3c5e40faaa3e
zvkemp <zvkemp@gmail.com>
2014-10-20 14:22:18 -0700

    custom meta serializer

--- ruby_app/app/models/metadata_delegate.rb
+++ ruby_app/app/models/metadata_delegate.rb
@@ -4,10 +4,26 @@ require 'set'
 # tags and key-value pairs
 #
 class MetadataDelegate < ActiveRecord::Base
+  validates :key, presence: true, uniqueness: true
 
   # These have the :meta_ prefix so as not to conflict with
   # ActiveRecord's :attributes method. 
   serialize :meta_tags,       Set
   serialize :meta_attributes, Hash
 
+  def tags
+    meta_tags
+  end
+
+  def tags=(tags)
+    self.meta_tags = Set.new(tags)
+  end
+
+  def as_json(*args)
+    super(*args).tap do |hash|
+      hash[:tags]       = hash.delete(:meta_tags, []).to_a
+      hash[:attributes] = hash.delete(:meta_attributes, {})
+    end
+  end
 end
+
