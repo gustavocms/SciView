1bc74ff75ff11a7d20991c7db50275d5b7a53c20
zvkemp <zvkemp@gmail.com>
2014-10-20 18:00:45 -0700

    better initializer

--- ruby_app/config/initializers/influx_db.rb
+++ ruby_app/config/initializers/influx_db.rb
@@ -1,3 +1,10 @@
 YAML.load(File.read(File.join(Rails.root, *%w[config influx_db.yml]))).tap do |config|
+
   INFLUX_DB_NAME = config[Rails.env]["name"]
+
+  unless InfluxDB::Client.new.get_database_list.any? {|db| db["name"] == INFLUX_DB_NAME }
+    InfluxDB::Client.new.create_database(INFLUX_DB_NAME)
+  end
 end
+
+Dataset.use_adapter(DatasetAdapters::InfluxAdapter)
