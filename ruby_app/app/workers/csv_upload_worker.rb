e1166e7faf80387bfc7ed1beccef9cf0764685c7
Gustavo Souza <gustavo.souza@toptal.com>
2014-10-22 21:11:51 -0200

    Debugging log

--- ruby_app/app/workers/csv_upload_worker.rb
+++ ruby_app/app/workers/csv_upload_worker.rb
@@ -2,6 +2,8 @@ class CsvUploadWorker
   include Sidekiq::Worker
 
   def perform(series_name, s3_path)
+    puts "Processing series #{series_name} file #{s3_path}"
+
     S3CsvToTempoDb.new(s3_path).tap do |csv|
       csv.series_name = series_name
       csv.save!
