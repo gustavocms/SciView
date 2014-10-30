805c50e5c57a72bba4a3b788b97487fedbd06fa6
zvkemp <zvkemp@gmail.com>
2014-10-30 15:55:58 -0700

    change worker to use more abstract csv importer

--- ruby_app/app/workers/csv_upload_worker.rb
+++ ruby_app/app/workers/csv_upload_worker.rb
@@ -2,7 +2,7 @@ class CsvUploadWorker
   include Sidekiq::Worker
 
   def perform(series_name, s3_path)
-    S3CsvToTempoDb.new(s3_path).tap do |csv|
+    S3CsvToDataset.new(s3_path).tap do |csv|
       csv.series_name = series_name
       csv.save!
       csv.wait_for_tempo_db
